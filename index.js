b5f65c257127f264a8d608c3b3f741db9ec11d32
mildlybrutal <akashsingu48@gmail.com>
2025-11-17 20:33:54 +0000

    lmao

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get("/",(req,res)=>{
     console.log("Hello")
 })
+app.get("/hello",(req,res)=>{
+    console.log("Hello bruv")
+})
 
 app.listen(8080,()=>{
     console.log("Server is listening on port 8080")
